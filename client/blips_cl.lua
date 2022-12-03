c40f288ca41675ca1bc9cbd11d6b9b004d9d6800
P Gergő <paldi95@gmail.com>
2022-12-03 12:24:20 +0100

    Add function

--- client/blips_cl.lua
+++ client/blips_cl.lua
@@ -1,8 +1,38 @@
 local BLIPS = nil
 
 BLIPS = {
-    Add = function(self)
-        
+    Add = function(self, id, coords, category, label, sprite, size, color)
+
+        if not id then
+            print("[^1ERROR^7] ^5ESX Blips^7 ID missing!")
+            return
+        end
+
+        if not coords then
+            print("[^1ERROR^7] ^5ESX Blips^7 coords missing!")
+            return
+        end
+
+        if not category then category = 'default' end
+
+        if not label then
+            label = 'unknown'..id
+        end
+
+        local blip = AddBlipForCoord(coords.x, coords.y, coords.z)
+        SetBlipSprite(blip, sprite or 1)
+        SetBlipScale(blip, size or 0.7)
+        SetBlipColour(blip, color or 1)
+        SetBlipAlpha(blip, 255)
+        SetBlipAsShortRange(blip, true)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentSubstringPlayerName(label)
+        EndTextCommandSetBlipName(blip)
+
+        BLIPS.Data[category][id] = {
+            blip = blip,
+            coords = coords
+        }
     end,
     Disable = function(self, category)
         
@@ -11,6 +41,9 @@ BLIPS = {
         
     end,
     Enable = function(self, category)
+        
+    end,
+    EnableAll = function(self)
         
     end,
     SetWayPoint = function(self)
@@ -22,7 +55,4 @@ BLIPS = {
 -- Handlers
 AddEventHandler('esx:onPlayerLogout', BLIPS.DisableAll)
 
-AddEventHandler('onResourceStart', function(resource)
-    if GetCurrentResourceName() ~= resource  then return end
-
-end)
+AddEventHandler('esx:playerLoaded', BLIPS.EnableAll)
